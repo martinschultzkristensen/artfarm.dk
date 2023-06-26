9e1d3c0c01cf53ee2cf2fc953817105fc69c613b
martinschultzkristensen <m4rtinsk@protonmail.com>
2023-06-26 13:45:28 +0200

    add php head&header to about.php

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="da">
+<?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'head.php';
+    ?>
+
+  <body class="bg-light">
+
+  <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'header.php';
+    ?>
+
+
+<!-- <div class="container-fluid;">
+  <div class="card text-white text-uppercase">  
+    <img src="./img/frikadelleMedSpons.jpg" alt="" class="card-img vh-100" style="object-fit:cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text"></span>Forestilling: "Frikadelle"</p>
+          <h1 class="card-title;">På Aprilfestival 28. April</h1>
+          <a href="/repertoire/ntwrkTW" class="btn btn-outline-light btn-lg">Info</a>        
+        </div>
+      </div>
+
+
+</div> -->
+<div class="container-fluid;">
+  <div class="card text-white text-uppercase">  
+    <img src="./img/terminusBwLayOut.png" alt="" class="card-img vh-100" style="object-fit:cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text"></span>Forestilling: "Terminus"</p>
+
+
+          <h1 class="card-title;">Turné 25. Aug. - 3. Sep.</h1>
+   <a href="/repertoire/terminus" class="btn btn-outline-light btn-lg">Info</a>        
+        </div>
+      </div>
+  </div>
+
+
+  <div class="card text-white text-uppercase">  
+    <img src="./img/ntvrkTW.jpg" alt="" class="card-img vh-100" style="object-fit:cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text"></span>Worskhop & netværk i Taiwan</p>
+          <h1 class="card-title;">Co-Laboratorie omkring censur</h1>
+          <a href="/repertoire/ntwrkTW" class="btn btn-outline-light btn-lg">Info</a>        
+        </div>
+      </div>
+  </div>
+
+
+  <div class="card text-white text-uppercase">  
+    <img src="./img/dmaskine_1.jpg" alt="" class="card-img vh-100" style="object-fit:cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text"><span class=""><img src="./img/taiwanTxt.png" width="115" height="45" class="d-inline-block align-text-bottom"></a></span> dance tour</p>
+          <h1 class="card-title;">March 2023<br>DANCEOMATIC - Jumping Islands</h1>
+          <a href="/repertoire/danceomatic" class="btn btn-outline-light btn-lg">Info</a>        
+        </div>
+      </div>
+  </div>
+
+  <div class="card text-white text-uppercase">  
+    <img src="./img/fb_fire.jpg" alt="" class="card-img vh-100" style="object-fit: cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text">Proof of concept <br> performed dec. 3rd. Kolding, DK</p>
+          <h1 class="card-title;">Fire Drum Dance</h1>
+          <a href="https://www.instagram.com/reel/Cl165H4JuQo/?utm_source=ig_web_copy_link" class="btn btn-outline-light btn-lg" target="_blank">Instagram</a>          
+        </div>
+      </div>
+  </div>
+
+  <div class="card text-white text-uppercase">  
+    <img src="./img/akryla_shan_structure1.jpg" alt="" class="card-img vh-100" style="object-fit: cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text">active performance</p>
+          <h1 class="card-title;">AKRYLARIUM</h1>
+          <a href="/repertoire/akrylarium" class="btn btn-outline-light btn-lg">Info</a>
+        </div>
+      </div>
+  </div>
+
+  <!-- 
+  <div class="card text-white text-uppercase">  
+    <img src="./img/kinaShow2BigScreen.jpg" alt="" class="card-img vh-100" style="object-fit:cover; filter: brightness(70%);">
+      <div class="card-img-overlay position-absolute">
+        <div class="position-absolute bottom-0 p-md-5 px-3 py-5">
+          <p class="card-text">performance i februar</p>
+          <h1 class="card-title;">traditionelt Kinesisk<br>show</h1>
+          <a href="#" class="btn btn-outline-light btn-lg">Info</a>
+        </div>
+      </div>
+  </div> 
+  -->
+
+  <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'footer.php';
+    ?>
+  
+  
+  <!-- <div class="d-sm-inline-flex p-2 bd-highlight bg-danger">I'm a flexbox container!</div> -->
+  <script src="./js/bootstrap.bundle.min.js"></script>
+</body>
+
+
+</html>
