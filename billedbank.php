e8073516376db47240dac4b753b13892f1181800
martinschultzkristensen <m4rtinsk@protonmail.com>
2023-08-06 11:14:20 +0200

    created Terminus picturebank

---
+++ billedbank.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="da">
+<?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'head.php';
+    ?>
+    
+<body>
+
+<?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'header.php';
+    ?>
+
+<section class="py-5 text-center container">
+      <div class="row pt-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light">Billedbank</h1>  
+        <div class="col">
+          <p>TERMINUS</p>
+        </div>
+      </div>
+    </section>
+    <div >
+        <img src="/img/terminusBwLayOut.png" alt="Billede 1" class="card-img vh-100" style="object-fit:cover;">
+        <img src="/img/terminusBWSplit.png" alt="Billede 2" class="card-img vh-100" style="object-fit:cover;">
+        <img src="/img/terminusTrainstation.jpg" alt="Billede 3">
+        <!-- Tilføj flere billeder her -->
+    </div>
+    
+    </div>
+</body>
+</html>
