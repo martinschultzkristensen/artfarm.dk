f4f9273b530e5eb05606f565b783ce10d2d4184d
martinschultzkristensen <m4rtinsk@protonmail.com>
2024-04-06 22:10:13 +0200

    link svg element from <object> tag

--- js/my_script.js
+++ js/my_script.js
@@ -4,14 +4,23 @@
  * Released under the MIT license
  */
 
-document.getElementById("Pil").addEventListener("animationend", function () {
+// Wait for the SVG document to be fully loaded
+document.getElementById("danceOmatic_logo").addEventListener("load", function () {
+  
+  // Access the SVG document
+  var svgDoc = this.contentDocument;
+
+svgDoc.getElementById("Pil").addEventListener("animationend", function () {
   console.log("Animationen af Pil er fuldført!"); // Tilføjet console.log()
   showFontPinkLetters();
   setTimeout(fadeInShadowEffect, 2200);
 });
+});
 
 function showFontPinkLetters() {
-  var letters = document.querySelectorAll(".letter");
+  var svgDoc = document.getElementById("danceOmatic_logo").contentDocument;
+
+  var letters = svgDoc.querySelectorAll(".letter");
   letters.forEach(function (letter, index) {
     var delay = index === letters.length - 11 ? 150 : 110; //delay for letter "O". nr 11 in the class letters
     setTimeout(function () {
@@ -20,7 +29,8 @@ function showFontPinkLetters() {
   });
 }
 
-function fadeInShadowEffect() {
-  var shadowElement = document.getElementById("shadeEffect");
+  function fadeInShadowEffect() {
+    var svgDoc = document.getElementById("danceOmatic_logo").contentDocument;
+  var shadowElement = svgDoc.getElementById("shadeEffect");
   shadowElement.classList.add("show");
 }
