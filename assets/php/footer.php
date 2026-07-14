58dab83aec1bd89415b6b5172f57a52ec69d5ea9
martinschultzkristensen <m4rtinsk@protonmail.com>
2026-07-14 12:28:31 +0200

    adding RegSyd logo in footer.php

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -4,19 +4,18 @@
       <div class="col-sm-12">
         <h2 class="featurette-heading">Support and <span class="text-muted">collaborators</span></h2>
         <div class="row">
-            <div class="col-md-2 col-7">
-              <img src="/img/support_logo/Altuglas.svg" alt="" srcset="" class="img-fluid p-1">
-              <img src="/img/support_logo/van_fonden_logo1.png" alt="" srcset="" class="img-fluid p-1">
+            <div class="col-md-3 col-7 p-3">
+              <img src="/img/support_logo/Region Syddanmark logo_sek_color.png" alt="" srcset="" class="img-fluid">
             </div>
-            <div class="col-md-1 col-4 mt-2">
-              <img src="/assets/svg_elements/GHT_BiberBlack.svg" alt="logo Gerhard Haubtmann Theater" class="img-fluid pt-sm-4 px-1">
-              <img src="/img/support_logo/hca_hvid_2024_dato-by-år.png" alt="Mai-Ti dance company logo" class="img-fluid pt-3">
-            </div>
-            <div class="col-md-1 col-3">
-              <img src="/img/support_logo/humlabFaceBlack.png" alt="HumanLab logo" class="img-fluid pt-4 px-2">
+            <div class="col-md-2 col-4 p-2">
+            <img src="/img/support_logo/van_fonden_logo1.png" alt="" srcset="" class="img-fluid p-1">  
+            <img src="/assets/svg_elements/GHT_BiberBlack.svg" alt="logo Gerhard Haubtmann Theater" class="img-fluid pt-sm-4 px-1">
             </div>
             <div class="col-md-1 col-3">
               <img src="https://d2typry64h97y6.cloudfront.net/wp-content/uploads/2023/01/24135746/Globus-Opstart_Logo_BLACK_RGB.png" alt="logo globus opstart" class="img-fluid d-inline-flex pt-3">
+            </div>
+            <div class="col-md-1 col-3">
+              <img src="/img/support_logo/humlabFaceBlack.png" alt="HumanLab logo" class="img-fluid pt-4 px-2">
             </div>
             <div class="col-md-1 col-3">
               <img src="/img/support_logo/logoMaiTiTransparrent.png" alt="Mai-Ti dance company logo" class="img-fluid pt-3">
@@ -24,6 +23,8 @@
             <div class="col-md-1 col-3 m-0">
               <img src="/img/support_logo/StKFond.png" alt="" srcset="" class="img-fluid">
             </div>
+
+            
            
             
         </div>
